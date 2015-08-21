515bc33c33b83afd21c3a9c8f7747ce17c696bbf
Aarne Kyppö <kyppoa@gmail.com>
2015-08-21 08:29:43 +0300

    Added possibility to insert an object into notification for its callback.

--- src/android/pushNotificationService.java
+++ src/android/pushNotificationService.java
@@ -37,8 +37,9 @@ public class pushNotificationService extends CordovaPlugin {
         if (action.equals("initialize")){
             String iconkey = args.getString(0);
             String icongroup = args.getString(1);
+
             if(iconkey != null && icongroup != null){
-              this.initialize(iconkey,icongroup, cb);
+              this.initialize(iconkey,icongroup);
             }
             else{
               cb.error("Icon key or icon group is not defined");
@@ -48,66 +49,50 @@ public class pushNotificationService extends CordovaPlugin {
         else if (action.equals("sendNotification")) {
             String title = args.getString(0);
             String message = args.getString(1);
-            if(title != null & message != null){
-              this.sendNotification(title,message, cb);
+            JSONObject data = args.getJSONObject(2);
+            if(title != null & message != null && data != null){
+              this.sendNotification(title,message,data);
             }
             else{
-              cb.error("Notification title or message is not defined");
+              cb.error("Notification title, message or dataobject is not defined");
             }
             return true;
         }
         return false;
     }
-    private void initialize(String iconkey, String icongroup, CallbackContext cb){
-        this.cb = cb;
+    private void initialize(String iconkey, String icongroup){
         this.iconkey = iconkey;
         this.icongroup = icongroup;
         this.activity = this.cordova.getActivity();
         this.context = this.activity.getApplicationContext();
         //If user have opened push notification, send data of notification to user
         Intent intent = ((CordovaActivity) this.activity).getIntent();
-        if(intent.hasExtra("extra")){ //Check if intent includes URL for a job. It will have URL if user have opened notification.
-          handleNotification(intent.getStringExtra("extra"));
+        if(intent.hasExtra("data")){ //Check if intent includes URL for a job. It will have URL if user have opened notification.
+          handleNotification(intent.getStringExtra("data"));
         }
         else{
-          PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
-          result.setKeepCallback(true);
-          cb.sendPluginResult(result);
+          sendPluginResult();
         }
     }
     private void handleNotification(String data)
     {
-        String extra = data;
-        JSONObject json_obj = new JSONObject();
-        Boolean keep_cb = false;
+        JSONObject jsondata;
         try{
-          json_obj.put("msg",extra);
-          keep_cb = true;
+          jsondata = new JSONObject(data);
+        }catch(JSONException je){
+          jsondata = new JSONObject();
+          cb.error("Given data is not valid");
         }
-        catch(JSONException je){
-
-        }
-        PluginResult result = new PluginResult(PluginResult.Status.OK,json_obj);
-        result.setKeepCallback(keep_cb);
-        cb.sendPluginResult(result);
+        sendPluginResult(jsondata);
     }
-    private void coolMethod(String message, CallbackContext cb) {
-        if (message != null && message.length() > 0) {
-            cb.success(message);
-            cb.success("And another call");
-        } else {
-            cb.error("Expected one non-empty string argument.");
-        }
-    }
-    private void sendNotification(String title, String message, CallbackContext cb){
-      this.cb = cb;
+    private void sendNotification(String title, String message, JSONObject data){
 
       NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context);
       mBuilder.setSmallIcon(context.getResources().getIdentifier(this.iconkey,this.icongroup,context.getPackageName()));
       mBuilder.setContentTitle(title);
       mBuilder.setContentText(message);
       Intent resultIntent = new Intent(context,this.activity.getClass());
-      resultIntent.putExtra("extra","extra");
+      resultIntent.putExtra("data",data.toString());
 
       TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
       stackBuilder.addParentStack(this.activity.getClass());
@@ -117,6 +102,14 @@ public class pushNotificationService extends CordovaPlugin {
       NotificationManager mNotificationManager = (NotificationManager) this.activity.getSystemService(context.NOTIFICATION_SERVICE);
       mNotificationManager.notify(1000, mBuilder.build());
 
+      sendPluginResult();
+    }
+    private void sendPluginResult(JSONObject jsondata){
+      PluginResult result = new PluginResult(PluginResult.Status.OK,jsondata);
+      result.setKeepCallback(true);
+      cb.sendPluginResult(result);
+    }
+    private void sendPluginResult(){
       PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
       result.setKeepCallback(true);
       cb.sendPluginResult(result);
--- www/pushNotificationService.js
+++ www/pushNotificationService.js
@@ -1,18 +1,15 @@
 var exec = require('cordova/exec');
 
 var pushNotificationService = {
-  initialize: function(){
+  initialize: function(success,error){
     this.service = "pushNotificationService";
     //Default icon to ensure that notificationcompat will get icon which is mandatory.
     this.icongroup = "drawable";
     this.iconkey = "icon";
-    exec(this.notificationsuccess,this.error,this.service,"initialize",[this.iconkey,this.icongroup]);
+    exec(success,error,this.service,"initialize",[this.iconkey,this.icongroup]);
   },
-  coolMethod: function(arg0) {
-      exec(this.notificationsuccess, this.error, this.service, "coolMethod", [arg0]);//Not so DRY. curry method will be implemented.
-  },
-  sendNotification: function(title, message){
-    exec(this.notificationsuccess, this.error, this.service, "sendNotification", [title, message]);
+  sendNotification: function(success,error, title, message,data){
+    exec(success, error, this.service, "sendNotification", [title, message,data]);
   },
   setIcon: function(key,group){
     if(key){
@@ -21,17 +18,7 @@ var pushNotificationService = {
     if(group){
       this.group = group;
     }
-  },
-  success: function(){
-  },
-  notificationsuccess: function(data){
-    if(data){
-      alert(data.msg);
-    }
-  },
-  error: function(errmessage){
-    alert(errmessage);
-  },
+  }
 };
 
 module.exports = pushNotificationService;
